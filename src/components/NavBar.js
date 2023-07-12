95c8704feaa1505990d2a9cc2a94870b1cea77e3
AlexSmall96 <alex.small739@gmail.com>
2023-07-12 13:37:45 +0000

    "Add routing and styles for nav links"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">Smart <i class="fa-solid fa-glasses"></i> Work</Navbar.Brand>
+        <NavLink className={styles.NavLink} to="/">
+            <Navbar.Brand href="#home">
+                Smart <i class="fa-solid fa-glasses"></i> Work
+            </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>Sign In</Nav.Link>
-              <Nav.Link>Sign Up</Nav.Link>
+              <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/">Home</NavLink>
+              <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">Sign In</NavLink>
+              <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">Sign Up</NavLink>
             </Nav>
         </Navbar.Collapse>
       </Container>
