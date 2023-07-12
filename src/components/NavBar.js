1f06a06f274091ac6b6ea71f81203399ffad5177
AlexSmall96 <alex.small739@gmail.com>
2023-07-12 13:10:47 +0000

    "Create App.module.css file, update font in App"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
         <Navbar.Brand href="#home">Smart <i class="fa-solid fa-glasses"></i> Work</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
+            <Nav className="ml-auto text-left">
               <Nav.Link>Home</Nav.Link>
               <Nav.Link>Sign In</Nav.Link>
               <Nav.Link>Sign Up</Nav.Link>
