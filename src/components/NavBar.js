03f5958ccb11d1adb97bb425d5a9f5fd6be8055b
AlexSmall96 <alex.small739@gmail.com>
2023-07-15 15:45:37 +0000

    "Add form submit and error handling"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,47 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import styles from '../styles/NavBar.module.css'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <NavLink className={styles.NavLink} to="/">
-            <Navbar.Brand href="#home">
-                Smart <i class="fa-solid fa-glasses"></i> Work
-            </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+          Smart <i className="fa-solid fa-glasses"></i> Work
+          </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-              <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/">Home</NavLink>
-              <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">Sign In</NavLink>
-              <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">Sign Up</NavLink>
-            </Nav>
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
+          </Nav>
         </Navbar.Collapse>
       </Container>
-   </Navbar>
-    )
-}
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
