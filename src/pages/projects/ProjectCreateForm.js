19c47e1ca3746ca6ff9f0545adbec4182a892df2
AlexSmall96 <alex.small739@gmail.com>
2023-07-17 15:54:37 +0000

    "Create handle change function"

--- src/pages/projects/ProjectCreateForm.js
+++ src/pages/projects/ProjectCreateForm.js
@@ -1,29 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 function ProjectCreateForm() {
+
+    const [projectData, setProjectData] = useState({
+        title:'',
+        description:'',
+        complexity:'Low',
+        startDate:'',
+        dueDate:'',
+    })
+
+    const {title, description, complexity, startDate, dueDate} = projectData
+
+    const handleChange = (event) => {
+        setProjectData({
+            ...projectData,
+            [event.target.name]: event.target.value
+        })
+    }
+
     return (
         <div>
             <Form>  
             <Form.Group controlId="title">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Project Title" />
+                <Form.Control 
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleChange} />
             </Form.Group>
             <Form.Group controlId="description">
                 <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Project Description" />
+                <Form.Control 
+                type="text"
+                name="description"
+                value={description}
+                onChange={handleChange} />
             </Form.Group>
             <Form.Group controlId="start-date">
                 <Form.Label>Start Date</Form.Label>
-                <Form.Control type="date" />
+                <Form.Control 
+                type="date"
+                name="start-date"
+                value={startDate}
+                onChange={handleChange} />
             </Form.Group>
             <Form.Group controlId="due-date">
                 <Form.Label>Due Date</Form.Label>
-                <Form.Control type="date" />
+                <Form.Control 
+                type="date"
+                name="due-date"
+                value={dueDate}
+                onChange={handleChange} />
             </Form.Group>
             <Form.Group controlId="complexity">
                 <Form.Label>Complexity</Form.Label>
-                <Form.Control as="select">
+                <Form.Control 
+                as="select"
+                name="complexity"
+                value={complexity}
+                onChange={handleChange}
+                >
                     <option>Low</option>
                     <option>Medium</option>
                     <option>High</option>
