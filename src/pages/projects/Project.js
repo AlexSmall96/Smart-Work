63c8e8c617236389ff326c6ab51780086c23756b
AlexSmall96 <alex.small739@gmail.com>
2023-07-25 21:49:40 +0000

    "Add project delete button to project component"

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useState } from 'react';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Button, Card, Media } from 'react-bootstrap';
+import { Modal, Button, Card, Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from '../../components/Avatar';
 import MemberCreateForm from './MemberCreateForm';
-import Task from '../../components/TaskCreateForm';
 import TaskCreateForm from '../../components/TaskCreateForm';
 import ProjectEditForm from './ProjectEditForm';
 
 const Project = ({projectData, members}) => {
-    
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === projectData.project_owner_username
 
@@ -31,6 +33,23 @@ const Project = ({projectData, members}) => {
                 <>
                     <MemberCreateForm projectId={projectData.project} title={projectData.title} memberProfileIds={members.map(member => member.profile)} />
                     <ProjectEditForm data={projectData} />
+                    <Button variant="primary" onClick={handleShow}>
+                        Delete Project
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                        <Modal.Title>Are you sure you want to delete {projectData.title}? This can't be undone.</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            No
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Yes
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                     <TaskCreateForm members={members} />
                 </>
             ): (
