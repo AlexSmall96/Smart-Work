c87e0e7c27aa04584f88f0dd68e03d67f68ec151
AlexSmall96 <alex.small739@gmail.com>
2023-08-05 13:37:29 +0000

    "Add task detail to tasks page"

--- src/pages/tasks/TasksPage.js
+++ src/pages/tasks/TasksPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Card } from 'react-bootstrap'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { Button, Card, Row, Col } from 'react-bootstrap'
+import { Link, useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import { axiosReq } from '../../api/axiosDefaults'
 
 const TasksPage = () => {
@@ -10,7 +10,7 @@ const TasksPage = () => {
   useEffect(() => {
     const fetchTasks = async () => {
         try {
-            const response = await axiosReq.get(`/tasks/?assigned_to_profile_id=${id}`)
+            const response = await axiosReq.get(`/tasks/?assigned_to__profile=${id}`)
             setTasks(response.data)
         } catch(err){
             console.log(err)
@@ -20,14 +20,31 @@ const TasksPage = () => {
   }, [id])
 
   return (
+    <>
     <Card>
         <Card.Header>Your Tasks</Card.Header>
+        <Card.Header>
+          <Row>
+            <Col>Description</Col>
+            <Col>Start Date</Col>
+            <Col>Due Date</Col>
+            <Col>Status</Col>
+          </Row>
+        </Card.Header>
         <Card.Body>
             {tasks.length?(
-                tasks.map(task => <p key={task.id}>{task.description}</p>)
+                tasks.map(task => 
+                <Row key={task.id}>
+                    <Col>{task.description}</Col>
+                    <Col>{task.start_date}</Col>
+                    <Col>{task.due_date}</Col>
+                    <Col>{task.status}</Col>
+                </Row>)
             ):('No results')}
         </Card.Body>
     </Card>
+      <Link to={`/projects/${id}`}><Button>Go to My Projects to edit your tasks.</Button></Link>
+    </>
   )
 }
 
