5fc59fdea4a68b4e196fa0198ae0738c8e8de8f8
AlexSmall96 <alex.small739@gmail.com>
2023-08-08 16:47:55 +0000

    "Add Asset spinner to projects page"

--- src/pages/projects/ProjectsPage.js
+++ src/pages/projects/ProjectsPage.js
@@ -4,17 +4,20 @@ import { axiosReq } from '../../api/axiosDefaults'
 import Project from './Project';
 import appStyles from '../../App.module.css'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
+import Asset from '../../components/Asset';
 
 const ProjectsPage = () => {
     const { id } = useParams();
     const [members, setMembers] = useState([])
+    const [hasLoaded, setHasLoaded] = useState(false);
 
     useEffect(() => {
         const fetchMembers = async () => {
           try {
             const response = await axiosReq.get(`/members/?profile=${id}`)
             setMembers(response.data)
+            setHasLoaded(true);
           } catch(err){
             console.log(err.response)
           }
@@ -22,13 +25,18 @@ const ProjectsPage = () => {
         fetchMembers()
       }, [id])
   return (
-    <div className={appStyles.background}>
-        {members.length?(
-            members.map(member => <Project key={member.id} projectData={member} />
-        )
-        ):(<>You're not currently a member of any projects. <Link to='/create'><Button variant="primary">Create a Project</Button></Link></>)
-        }
-    </div>
+    hasLoaded?(
+      <div className={appStyles.background}>
+      {members.length?(
+          members.map(member => <Project key={member.id} projectData={member} />
+      )
+      ):(<>You're not currently a member of any projects. <Link to='/create'><Button variant="primary">Create a Project</Button></Link></>)
+      }
+  </div>
+    ):(<Container className={appStyles.Content}>
+      <Asset spinner />
+    </Container>)
+
   )
 }
 
