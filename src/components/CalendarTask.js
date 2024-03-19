510c3074ed64c0159d733c1f3fb922ffd36acf03
AlexSmall96 <alex.small739@gmail.com>
2024-03-19 15:02:20 +0000

    Delete and restore package-lock.json

--- src/components/CalendarTask.js
+++ src/components/CalendarTask.js
@@ -23,23 +23,23 @@ const CalendarTask = ({task, projectData, projLength, yearView, year, month, mon
         31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     ]
     // Calculate length of task in relation to project
-    const getTaskLength = (taskStartDate, taskDueDate, projStartDate, projDueDate) => {
+    const getTaskLength = (taskStartDate, taskDueDate) => {
         let length;
         let rangeStart = yearView ? (new Date(`01/01/${year}`).getTime()):(new Date(`${monthNum + 1}/01/${year}`).getTime())
         let rangeEnd = yearView ? (new Date(`12/31/${year}`).getTime()):(new Date(`${monthNum + 1}/${daysInMonth[monthNum]}/${year}`).getTime() )
-        // Sandwhiched in year
+        // Sandwhiched in range
         if (rangeStart <= taskStartDate && taskDueDate <= rangeEnd){
             length = 100 * (taskDueDate - taskStartDate + (yearView ? (0):(1000 * 3600 * 24))) / projLength
         // 0 length case
         } else if (rangeEnd < taskStartDate || taskDueDate < rangeStart){
             length = 0
-        // Full year case
+        // Full range case
         } else if (taskStartDate <= rangeStart && rangeEnd <= taskDueDate){
             length = 100
-        // Starts before year
+        // Starts before range
         } else if (taskStartDate <= rangeStart && taskDueDate <= rangeEnd){
             length = 100 * (taskDueDate - rangeStart + (yearView ? (0):(1000 * 3600 * 24))) / projLength
-        // Ends after year
+        // Ends after range
         } else if (taskStartDate >= rangeStart && taskDueDate >= rangeEnd){
             length = 100 * (rangeEnd - taskStartDate + (yearView ? (0):(1000 * 3600 * 24))) / projLength
         }
@@ -47,7 +47,7 @@ const CalendarTask = ({task, projectData, projLength, yearView, year, month, mon
     }
 
     // Calculate time between task start date and project start date
-    const getTaskOffset = (taskStartDate, projStartDate, projDueDate) => {
+    const getTaskOffset = (taskStartDate, projStartDate) => {
         let monthNum = months.findIndex((element) => element === month)
         let rangeStart = yearView ? (new Date(`${monthNum + 1}/01/${year}`)):(new Date(`${monthNum + 1}/01/${year}`))   
         if (taskStartDate <= rangeStart){
@@ -82,12 +82,8 @@ return (
             width:`${!emptyProject ? (
                 getTaskLength(
                     new Date(task.start_date).getTime(), 
-                    new Date(task.due_date).getTime(), 
-                    new Date(projectData.start_date.slice(0,10)).getTime(), 
-                    new Date(projectData.due_date.slice(0,10)).getTime())):(
+                    new Date(task.due_date).getTime())):(
                 getTaskLength(
-                    new Date(projectData.start_date.slice(0,10)).getTime(), 
-                    new Date(projectData.due_date.slice(0,10)).getTime(), 
                     new Date(projectData.start_date.slice(0,10)).getTime(), 
                     new Date(projectData.due_date.slice(0,10)).getTime())    
                 )
@@ -95,8 +91,7 @@ return (
             margin:`0 0 0 ${!emptyProject ? (
                 getTaskOffset(
                     new Date(task.start_date).getTime(),
-                    new Date(projectData.start_date.slice(0,10)).getTime(), 
-                    new Date(projectData.due_date.slice(0,10)).getTime())):(
+                    new Date(projectData.start_date.slice(0,10)).getTime())):(
                 0)
             }%`,
             height:'18px',
@@ -104,7 +99,7 @@ return (
             display:'block',
             fontSize:'small',
             backgroundColor:`${!emptyProject ? (getColor(task.status, task.due_date)):('lightgray')}`,
-            overflow:'hidden'
+            overflow:'hidden',
         }}
     >{!emptyProject ? (task.description):('')}</div>
 </OverlayTrigger>
